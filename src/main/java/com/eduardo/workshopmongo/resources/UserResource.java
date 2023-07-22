e9dc5aa5943e512f02eb37bd9bbc9a705ba5482a
EduardoRSantos <eduardoow6@gmail.com>
2023-07-22 18:03:36 -0300

    Operacao de instanciacao da base de dados

--- src/main/java/com/eduardo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/eduardo/workshopmongo/resources/UserResource.java
@@ -1,24 +1,25 @@
 package com.eduardo.workshopmongo.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eduardo.workshopmongo.entities.User;
+import com.eduardo.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
-    
+
+    @Autowired
+    private UserService service;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Silva", "maria@gmail.com");
-        User alex = new User("2", "Alex", "alex@gmail.com");
-        return ResponseEntity.ok().body(Arrays.asList(maria, alex));
+        return ResponseEntity.ok().body(service.findAll());
     }
 }
